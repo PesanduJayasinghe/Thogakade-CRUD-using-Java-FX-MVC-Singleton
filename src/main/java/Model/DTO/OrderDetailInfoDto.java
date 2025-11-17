2d5806d73933e567c052be3add4ac107818fb630
PesanduJayasinghe <pesanduse@gmail.com>
2025-11-17 13:58:54 +0530

    Created OrderDetailInfoDto

---
+++ src/main/java/Model/DTO/OrderDetailInfoDto.java
@@ -0,0 +1,17 @@
+package Model.DTO;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class OrderDetailInfoDto {
+
+    private String orderId;
+    private String itemCode;
+    private int orderQty;
+    private String discount;
+
+}
